c3a5311fde8b57f06647f41e6a3472f7bd8a0ecc
DoveyLoveyCora <DoveyLoveyCora@163.com>
2023-03-14 21:47:19 +0800

    feat: required

---
+++ 02759-medium-requiredbykeys.ts
@@ -0,0 +1,46 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface User {
+  name?: string
+  age?: number
+  address?: string
+}
+
+interface UserRequiredName {
+  name: string
+  age?: number
+  address?: string
+}
+
+interface UserRequiredNameAndAge {
+  name: string
+  age: number
+  address?: string
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, 'name'>, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, 'name' | 'age'>, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  // @ts-expect-error
+  Expect<Equal<RequiredByKeys<User, 'name' | 'unknown'>, UserRequiredName>>,
+]
+
+
+// ============= Your Code Here =============
+type RequiredByKeys<T, K extends keyof T = keyof T> = Merge<{
+  [P in K]-?: T[P]
+} & {
+  [P in keyof Omit<T, K>]?: T[P]
+}>
+
+type Merge<T> = {
+  [P in keyof T]: T[P]
+}
+
+// type RequiredByKeys<T, K extends keyof T = keyof T> = Merge<{
+//   [Key in keyof T as Key extends K ? Key : never]-?: T[Key]
+// } & {
+//   [Key in keyof T as Key extends K ? never : Key]: T[Key]
+// }>
